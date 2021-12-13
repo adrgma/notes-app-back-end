97878da0e3dad363fe8a45e3f1afac98e808e050
adrgma <arga.aditia@gmail.com>
2021-12-14 06:11:38 +0700

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -4,30 +4,45 @@ const routes = (handler) => [
     method: 'POST',
     path: '/notes',
     handler: handler.postNoteHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     // tampilkan semua note
     method: 'GET',
     path: '/notes',
     handler: handler.getAllNotesHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     // tampilkan note sesuai Id
     method: 'GET',
     path: '/notes/{id}',
     handler: handler.getNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     // edit note sesuai Id
     method: 'PUT',
     path: '/notes/{id}',
     handler: handler.putNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     // hapus note sesuai Id
     method: 'DELETE',
     path: '/notes/{id}',
     handler: handler.deleteNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
 ];
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+const authentications = require('./api/authentications');
 const notes = require('./api/notes');
 const users = require('./api/users');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 const NotesService = require('./services/postgres/NotesService');
 const UsersService = require('./services/postgres/UserService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 const NotesValidator = require('./validator/notes');
 const UsersValidator = require('./validator/users');
 require('dotenv').config();
@@ -10,6 +15,8 @@ require('dotenv').config();
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,6 +25,29 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -33,6 +63,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
