e8a1f558ee507ce66e75dd7e39db0d1b5cc23aa2
adrgma <arga.aditia@gmail.com>
2021-12-27 20:11:32 +0700

    add collaboration temporary

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const authentications = require('./api/authentications');
+const collaborations = require('./api/collaborations');
 const notes = require('./api/notes');
 const users = require('./api/users');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
 const NotesService = require('./services/postgres/NotesService');
 const UsersService = require('./services/postgres/UserService');
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
+const CollaborationsValidator = require('./validator/collaborations');
 const NotesValidator = require('./validator/notes');
 const UsersValidator = require('./validator/users');
 require('dotenv').config();
 
 const init = async () => {
-  const notesService = new NotesService();
+  const collaborationsService = new CollaborationsService();
+  const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
 
@@ -72,6 +76,14 @@ const init = async () => {
         usersService,
         tokenManager: TokenManager,
         validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        notesService,
+        validator: CollaborationsValidator,
       },
     },
   ]);
